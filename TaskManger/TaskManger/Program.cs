a4c9c72f8e64c9a303bf9ce613ac6a50697452cd
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-12-14 17:45:49 +0100

    Add files via upload

---
+++ TaskManger/TaskManger/Program.cs
@@ -0,0 +1,125 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace TaskManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List all running tasks
+            Process[] processes = Process.GetProcesses();
+          WriteLine("List of all running tasks: ");
+            foreach (Process process in processes)
+            {
+                WriteLine(process.ProcessName);
+            }
+            WriteLine();
+
+            
+            //Kill any task
+            WriteLine("Enter the name of the process to kill: ");
+
+            try
+            {
+                string processName = ReadLine();
+                foreach (Process process in processes)
+                {
+                    if (process.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        process.Kill(true);
+                        break;
+                    }
+                }
+                WriteLine($"Process {processName} killed successfully!");
+                WriteLine();
+            }
+            catch (Exception ex)
+            {
+
+                WriteLine(ex.Message);
+            }
+
+            //start
+
+
+/*            //Start a new process
+            WriteLine("Enter the name of the process to start: ");
+            try
+            {
+                string processNameToStart = ReadLine();
+                Process.Start(processNameToStart);
+                WriteLine($"Process {processNameToStart} started successfully!");
+            }
+            catch (Exception ex)
+            {
+
+                WriteLine(ex.Message);
+            }
+            WriteLine();*/
+           
+
+
+          /*  //Create a custom process
+            Process customProcess = new Process();
+            WriteLine("Enter the name of the custom process to start: ");
+            try
+            {
+                customProcess.StartInfo.FileName = ReadLine();
+                WriteLine("Enter the command line arguments of the custom process to start: ");
+                customProcess.StartInfo.Arguments = ReadLine();
+                customProcess.Start();
+                WriteLine($"Custom process {customProcess.StartInfo.FileName} started successfully!");
+            }
+            catch (Exception ex)
+            {
+
+                WriteLine(ex.Message);
+            }
+            WriteLine();*/
+
+            //Create a thread
+            Thread thread = new Thread(Secondary);
+            thread.Start();
+            WriteLine($" {thread.ManagedThreadId} Thread started successfully!");
+            WriteLine();
+
+            //Create a background thread
+            Thread backgroundThread = new Thread(Secondary);
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+            WriteLine($"Background thread {backgroundThread.Name} started successfully!");
+            WriteLine();
+
+            // check if threads are alive
+            WriteLine($"Thread {thread.Name} is {(thread.IsAlive ? "alive" : "not alive")}");
+            WriteLine($"Background thread {backgroundThread.Name} is {(backgroundThread.IsAlive ? "alive" : "not alive")}");
+            ReadKey();
+        }
+
+        private static void Secondary(object threadName)
+        {
+            WriteLine("Secondary thread started!, {0}", threadName);
+            //Do some work here
+            WriteLine("secondary thread finished!");
+        }
+     
+        
+
+/*        public static int Start(string processName)
+        {
+            var process =
+                Process.Start(processName);
+            return
+                process.Id;
+        }*/
+    }
+}
---
+++ TaskManger/TaskManger/task.cs
@@ -0,0 +1,48 @@
+﻿/*using System.Diagnostics;
+
+namespace TaskManger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ListAllRunningProcesses();
+        }
+
+        static void ListAllRunningProcesses()
+        {
+            // Get all the processes on the local machine, ordered by
+            // PID.
+
+            var runningProcs = from proc
+                               in Process.GetProcesses(".")
+                               orderby proc.Id
+                               select proc;
+            // Print out PID and name of each process.
+            foreach (var p in runningProcs)
+            {
+                string info = $"-> PID: {p.Id}\tName: {p.ProcessName}";
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("************************************\n");
+        }
+    }
+
+    class TaskManger
+    {
+        //list all running task
+        //kill all the task or any of them
+        //start a task
+        //create custom process and kill it //name the process and spin it up
+        //create custom thread and kill it
+        //create background thread
+        //check if they are alive
+
+
+
+    }
+
+
+}
+*/
